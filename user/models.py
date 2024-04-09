abebeffca1d8549be171f614e43a9a5a01d9f816
Mijashh <amijash@gmail.com>
2024-04-09 10:34:09 +0545

    feat: create model CustomUser and its CustomUserManager

---
+++ user/models.py
@@ -0,0 +1,40 @@
+from typing import Any
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    password = models.CharField(max_length=20)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self) -> Any:
+        return self.email
+
+    def has_perm(self, perm, obj=None) -> Any:
+        return self.is_superuser
+
+    def has_module_perms(self, app_label) -> Any:
+        return self.is_superuser
+
