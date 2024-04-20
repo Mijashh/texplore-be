f4e881a94bbac4cc7ae927147278d678b65566ba
Mijashh <amijash@gmail.com>
2024-04-20 09:41:56 +0545

    feat: handle validation for user login

--- user/views.py
+++ user/views.py
@@ -11,7 +11,7 @@ from .serializers import CustomUserSerializer
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes=[permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return CustomUser.objects.filter(id=self.request.user.id)
@@ -22,30 +22,35 @@ class CustomUserViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [permissions.IsAuthenticated]
         return [permission() for permission in permission_classes]
-    
+
+
 class AuthViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_permissions(self):
-        if self.action == 'login':
+        if self.action == "login":
             return [permissions.AllowAny()]
-        elif self.action == 'logout':
+        elif self.action == "logout":
             return [permissions.IsAuthenticated()]
         else:
             return super().get_permissions()
 
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=["post"])
     def logout(self, request):
         try:
             token = Token.objects.get(user=request.user)
             token.delete()
             logout(request)
         except (AttributeError, Token.DoesNotExist):
-            pass
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    
-    @action(detail=False, methods=['post'])
+            return Response(
+                {"message": "You are not logged in"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return Response(
+            {"message": "Logged Out Successfully!!!"}, status=status.HTTP_200_OK
+        )
+
+    @action(detail=False, methods=["post"])
     def login(self, request):
         email = request.data.get("email")
         password = request.data.get("password")
@@ -57,16 +62,18 @@ class AuthViewSet(viewsets.ViewSet):
             )
 
         user = authenticate(request, email=email, password=password)
+        email_exists = CustomUser.objects.filter(email=email).exists()
 
-        if user is None:
+        if user is None and not email_exists:
             return Response(
-                {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+                {"email": ["Invalid email!!!"]}, status=status.HTTP_401_UNAUTHORIZED
             )
-
+        elif user is None and email_exists:
+            return Response(
+                {"password": ["Incorrect password!!!"]},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
         Token.objects.filter(user=user).delete()
 
         token = Token.objects.create(user=user)
-
         return Response({"token": token.key}, status=status.HTTP_200_OK)
-
-
