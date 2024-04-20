c4b24cb723a3d00048f5c0d5c600a0a9e2a2745c
Mijashh <amijash@gmail.com>
2024-04-20 09:39:50 +0545

    feat: create new models Category, Cart and CartItem

--- product/models.py
+++ product/models.py
@@ -1,16 +1,53 @@
 from django.conf import settings
-from django.core.validators import MinValueValidator
 from django.db import models
 
 
-class Product(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    quantity= models.IntegerField()
-    rate= models.FloatField(validators=[MinValueValidator(1),])
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=264)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(blank=True, null=True)
+    description = models.TextField()
+    rating = models.JSONField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    session_id = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return "Cart:  {}".format(self.user) if self.user else "Cart:  {}".format(self.session_id)
+
+    def calculate_total(self):
+        total = 0
+        for item in self.cart_items.all():
+            total += float(item.product.price) * float(item.quantity)
+        total = round(total, 2)
+        self.total = total
+        self.save()
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cart_items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.product.title + " - " + str(self.cart.user)
+
+
