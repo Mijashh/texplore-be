118034baf2a697d5bedc4382f72fb5d540df3a67
Mijashh <amijash@gmail.com>
2024-04-20 09:44:33 +0545

    chore: migrations

--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -21,6 +21,8 @@ class Migration(migrations.Migration):
                 ('is_active', models.BooleanField(default=True)),
                 ('is_staff', models.BooleanField(default=False)),
                 ('is_superuser', models.BooleanField(default=False)),
+                ('contact_no', models.CharField(default=False, max_length=10)),
+                ('name', models.CharField(default=False, max_length=100)),
             ],
             options={
                 'abstract': False,
